7c7492334d46cf8a72cc62eeb8bd3970205998b0
ch4mpy <ch4mp@c4-soft.com>
2025-02-27 12:49:04 -1000

    Custom reactive authorized client manager in a profile

--- rest-showcase-chouette-api-webflux-addons/src/main/java/com/c4soft/showcase/rest/RestConfiguration.java
+++ rest-showcase-chouette-api-webflux-addons/src/main/java/com/c4soft/showcase/rest/RestConfiguration.java
@@ -2,11 +2,25 @@ package com.c4soft.showcase.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.c4_soft.springaddons.rest.HttpExchangeProxyFactoryBean;
 
 @Configuration
 public class RestConfiguration {
+
+  @Profile("custom-authorized-client-manager")
+  @Bean
+  ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+      ReactiveClientRegistrationRepository clientRegistrationRepository,
+      ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+    return new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+        clientRegistrationRepository, authorizedClientService);
+  }
 
   @Bean
   BiduleApi biduleApi(WebClient biduleClient) throws Exception {
