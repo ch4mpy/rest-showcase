5464ccbfc9df108880b2f6042f3f770d025f1d99
ch4mpy <ch4mp@c4-soft.com>
2025-02-27 16:54:51 -1000

    Use AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager

--- rest-showcase-reactor-spring-cloud-stream-kafka-webflux/src/main/java/com/c4soft/showcase/rest/infrastructure/config/ChronosClientConfig.java
+++ rest-showcase-reactor-spring-cloud-stream-kafka-webflux/src/main/java/com/c4soft/showcase/rest/infrastructure/config/ChronosClientConfig.java
@@ -1,38 +1,40 @@
 package com.c4soft.showcase.rest.infrastructure.config;
 
-import generated.com.c4soft.showcase.rest.infrastructure.rest.chronos.TaskManagerClient;
-import generated.com.c4soft.showcase.rest.infrastructure.rest.chronos.invoker.ApiClient;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.net.URI;
+import generated.com.c4soft.showcase.rest.infrastructure.rest.chronos.TaskManagerClient;
+import generated.com.c4soft.showcase.rest.infrastructure.rest.chronos.invoker.ApiClient;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
 @Configuration
 @Validated
-@ConfigurationProperties(prefix = "worker.chronos")
 public class ChronosClientConfig {
 
-    @NotNull
-    private URI url;
+  @Bean
+  ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+      ReactiveClientRegistrationRepository clientRegistrationRepository,
+      ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+    return new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+        clientRegistrationRepository, authorizedClientService);
+  }
 
-    @Bean
-    public ApiClient chronosApiClient(WebClient.Builder chronosClientBuilder) {
-        var webClient = chronosClientBuilder.clone();
+  @Bean
+  public ApiClient chronosApiClient(WebClient.Builder chronosClientBuilder) {
+    return new ApiClient(chronosClientBuilder.build());
+  }
 
-        return new ApiClient(webClient.build()).setBasePath(url.toString());
-    }
-
-    @Bean
-    public TaskManagerClient taskManagerClient(ApiClient apiClient) {
-        return new TaskManagerClient(apiClient);
-    }
+  @Bean
+  public TaskManagerClient taskManagerClient(ApiClient apiClient) {
+    return new TaskManagerClient(apiClient);
+  }
 
 }
