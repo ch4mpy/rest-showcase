1eeea619759311bff9c30cc659aaf88480d7e43c
ch4mpy <ch4mp@c4-soft.com>
2025-03-22 13:30:57 -1000

    Spring profile to serve machin API with self-signed SSL

--- rest-showcase-chouette-api-servlet-addons/src/test/java/com/c4soft/showcase/rest/ChouetteApiServletAddonsApplicationTests.java
+++ rest-showcase-chouette-api-servlet-addons/src/test/java/com/c4soft/showcase/rest/ChouetteApiServletAddonsApplicationTests.java
@@ -2,12 +2,15 @@ package com.c4soft.showcase.rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class ChouetteApiServletAddonsApplicationTests {
+  @MockitoBean
+  JwtDecoder jwtDecoder;
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
 }
